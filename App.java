91704e917c4ed6980f2fde272de4b8bacfc88889
rahul <106728652+rahulraju3107@users.noreply.github.com>
2025-01-15 18:30:57 +1100

    Add files via upload
    First time programming in Eclipse

---
+++ App.java
@@ -0,0 +1,25 @@
+package App;
+import javax.swing.*;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		int boardWidth = 360;
+		int boardHeight = 640;
+		
+		JFrame frame = new JFrame("Flappy Bird");
+//		frame.setVisible(true);
+		frame.setSize(boardWidth, boardHeight);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		FlappyBird flappybird = new FlappyBird();
+		frame.add(flappybird);
+		frame.pack(); // so the colour doesnt include title bar
+		flappybird.requestFocus();
+		frame.setVisible(true);
+	}
+
+}
